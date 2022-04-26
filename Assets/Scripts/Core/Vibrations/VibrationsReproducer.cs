94b167448d498d0c3e4f92e33ddc1e57462a2dd7
Roman <75870215+R0tmayer@users.noreply.github.com>
2022-04-26 13:21:39 +1000

    Initial commit

---
+++ Assets/Scripts/Core/Vibrations/VibrationsReproducer.cs
@@ -0,0 +1,23 @@
+﻿using Lofelt.NiceVibrations;
+using MoreMountains.FeedbacksForThirdParty;
+using UnityEngine;
+
+namespace Core.Vibrations
+{
+    public class VibrationsReproducer
+    {
+        private const string VibrationsPrefsKey = nameof(VibrationsPrefsKey);
+
+        public bool Enabled
+        {
+            get => PlayerPrefs.GetInt(VibrationsPrefsKey, 1) == 1;
+            set => PlayerPrefs.SetInt(VibrationsPrefsKey, value ? 1 : 0);
+        }
+
+        public void TryPlayHaptic(HapticPatterns.PresetType hapticPreset)
+        {
+            if (Enabled)
+                HapticPatterns.PlayPreset(hapticPreset);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# Template for hyper-casual games
+## Unity Engine 2020.3.20f1
+
+## Plugins and Packages
+- URP
+- Cinemachine
+- TextMeshPro
+- UI Scaling Toolkit
+- DI Framework
+
+## Ready-to-use features:
+- Input Swipe Receiver
+- Input Joystick Receiver
+- Hierarchy Highlighter
+- Upgrades system
+- Vibrations system
+
+## Assets:
+- ODIN Inspector
+- DOTween
+- FEEL & NiceVibrations
+- Graphy - Ultimate Stats Monitor
+- Better UI
+- MonKey Commander
+- Asset Hunter Pro
+- Highlight Plus
